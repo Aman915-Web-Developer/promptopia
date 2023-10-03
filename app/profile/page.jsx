6c7c2f6735397fa67edb31e98a0966a4d5f11ad8
a0g0jk9 <aman.goyal0@walmart.com>
2023-10-03 14:22:11 +0530

    first draft

---
+++ app/profile/page.jsx
@@ -0,0 +1,53 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/profile";
+
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+    if (session?.user.id) {
+      fetchPosts();
+    }
+  });
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = () => async (post) => {
+    try {
+      await fetch(`/api/prompt/${post._id.toString()}`, {
+        method: "DELETE",
+      });
+
+      const filteredPosts = posts.filter((p) => p.id !== post._id);
+      setPosts(filteredPosts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
